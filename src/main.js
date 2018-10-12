12a82a9fc645b5be920a95ef52501e627c33db92
ChenLiang <236338364@qq.com>
2018-10-12 23:48:00 +0800

    element-ui 按需引入

--- src/main.js
+++ src/main.js
@@ -4,12 +4,56 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import { Pagination,
+  Dialog,
+  Menu,
+  Submenu,
+  MenuItem,
+  Input,
+  Select,
+  Option,
+  Button,
+  Table,
+  TableColumn,
+  DatePicker,
+  TimeSelect,
+  TimePicker,
+  Popover,
+  Breadcrumb,
+  BreadcrumbItem,
+  Tabs,
+  Icon,
+  Loading,
+  MessageBox,
+  Message } from 'element-ui'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
-Vue.use(ElementUI)
+Vue.use(Pagination);
+Vue.use(Dialog);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(Input);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(Button);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(DatePicker);
+Vue.use(TimeSelect);
+Vue.use(TimePicker);
+Vue.use(Popover);
+Vue.use(Breadcrumb);
+Vue.use(BreadcrumbItem);
+Vue.use(Tabs);
+Vue.use(Icon);
+
+Vue.use(Loading.directive);
+
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$message = Message;
 
 /* eslint-disable no-new */
 new Vue({
